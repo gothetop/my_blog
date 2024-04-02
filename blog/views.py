75b335c358b82b7484b32d1918b8fac9468d6228
liushijia <3020422083@qq.com>
2024-04-02 19:22:53 +0800

    将函数视图优化为了类视图

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from blog.models import Tag, Post, Category
 from config.models import SideBar
 
-
+"""
+# 函数视图如下：
 def post_list(request, category_id=None, tag_id=None):
-    """文章列表视图"""
+    # 文章列表视图
     category = None
     tag = None
     if tag_id:
@@ -26,7 +28,7 @@ def post_list(request, category_id=None, tag_id=None):
 
 
 def post_detail(request, post_id):
-    """文章详情视图"""
+    # 文章详情页
     post = Post.get_post_detail(post_id)
     context = {
         'post': post,
@@ -34,3 +36,68 @@ def post_detail(request, post_id):
     }
     context.update(Category.get_navs())
     return render(request, "blog/detail.html", context)
+"""
+
+
+class CommonViewMixin:
+    """通用类：评论，上下导航栏"""
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+
+# 类视图如下
+class IndexView(CommonViewMixin, ListView):
+    """首页类视图"""
+    queryset = Post.latest_posts()
+    paginate_by = 1  # 一页展示五条数据
+    context_object_name = "post_list"  # 修改在模板中的对象名
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    """分类页面视图，继承自首页"""
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')  # 在url请求链接中找到category_id参数
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写页面获取到的数据集, 根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')   # self.kwargs指的是url的请求链接中带来的东西
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    """标签页，继承于首页类视图"""
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写数据集，根据标签进行筛选"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    """文章详情页，继承通用类和DetailView类"""
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = "post"
+    pk_url_kwarg = 'post_id'
